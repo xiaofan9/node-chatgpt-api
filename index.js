079b6972278ea06c6416592960907615912e3206
sifan <718662552@qq.com>
2023-04-02 02:46:35 +0800

    fix: 增加配置文件

--- index.js
+++ index.js
@@ -1,6 +1,1 @@
-import ChatGPTClient from './src/ChatGPTClient.js';
-import ChatGPTBrowserClient from './src/ChatGPTBrowserClient.js';
-import BingAIClient from './src/BingAIClient.js';
-
-export { ChatGPTClient, ChatGPTBrowserClient, BingAIClient };
-export default ChatGPTClient;
+import "./bin/server.js"
